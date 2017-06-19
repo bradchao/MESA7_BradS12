8b43017d4fff2501c16ab88c52c2fb4dc3d836ee
iii <iii@userteki-MacBook.local>
2017-06-19 11:15:34 +0800

    v2

--- BradS12/BradAPI.swift
+++ BradS12/BradAPI.swift
@@ -30,3 +30,36 @@ class Brad03 : Super1, Brad01, Brad02 {
     func m6(x:Int) -> Double {return 1.0}
 }
 
+protocol Brad04: Brad01, Brad02 {
+    func m7()
+}
+
+class Brad05 : Brad04 {
+    func m1(){}
+    func m2() -> Int {return 1}
+    func m3(x:Int) -> Double {return 1.0}
+    func m4(){}
+    func m5() -> Int {return 1}
+    func m6(x:Int) -> Double {return 1.0}
+    
+    func m7(){}
+}
+
+protocol Brad06 {
+    var x : Int {get set}
+    var y : Int {get}
+}
+
+class Brad07 : Brad06 {
+    var x: Int
+    var y: Int = 4
+    init(x : Int){
+        self.x = x
+    }
+}
+
+
+
+
+
+
